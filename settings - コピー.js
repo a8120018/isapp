d8319f04b2d5a090efcdcb6b45d3f2b6a507e4a4
Otoha Ishioka <a8120018@gsuite.si.aoyama.ac.jp>
2022-05-06 16:01:17 +0900

    settingsをコピー

---
+++ settings - コピー.js
@@ -0,0 +1,8 @@
+var settings = {};
+
+settings.baseurl = 'https://dbkiso.si.aoyama.ac.jp/oursql/';
+settings.apiurl = settings.baseurl + 'api/';
+settings.db = 'isdb';
+settings.userid = 'isp2021';
+settings.password = 'omuice';
+
